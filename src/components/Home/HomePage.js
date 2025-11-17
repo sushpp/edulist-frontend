24b12899cbf82611a5f3100850603b1e60b183a2
sushpp <sppawarsppawar6@gmail.com>
2025-11-17 14:49:13 +0530

    Fix the admin dashboard

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,3 @@
-// src/pages/HomePage.js
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { instituteService } from '../../services/institute';
